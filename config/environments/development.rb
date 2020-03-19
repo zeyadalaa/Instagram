2810374ac96636ab995a6215cbf2da1f5d9044c6
Zeyad Alaa <zeyad.alaaeldin1998@gmail.com>
2020-03-19 20:16:11 +0200

    Instagram-7 : Upload image

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -53,7 +53,7 @@ Rails.application.configure do
 
   # Suppress logger output for asset requests.
   config.assets.quiet = true
-
+  Paperclip.options[:command_path] = "/usr/bin/"
   # Raises error for missing translations.
   # config.action_view.raise_on_missing_translations = true
 
